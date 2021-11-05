ac1c424a8a88188b041ef379137578f8fb955044
Kevin Espinoza <K.espinoza1012@gmail.com>
2021-11-04 22:45:37 -0700

    Final Version

--- scripts/firebaseconfig.js
+++ scripts/firebaseconfig.js
@@ -2,21 +2,22 @@
     'use strict';        
     var App = window.App || {};
     // Your web app's Firebase configuration
-              // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-              var FirebaseConfig = {
-                apiKey: "AIzaSyD-KGG2u0Cb65rOPir3w0Ra1IOjM1h69nc",
-                authDomain: "coffeerun-56c9b.firebaseapp.com",
-                projectId: "coffeerun-56c9b",
-                storageBucket: "coffeerun-56c9b.appspot.com",
-                messagingSenderId: "47895541782",
-                appId: "1:47895541782:web:45afd992717fef9064dd37",
-                measurementId: "G-7CLEK2MBTK"
-              };
-            
-              App.FirebaseConfig = FirebaseConfig;
-              // Initialize Firebase
-              firebase.initializeApp(App.FirebaseConfig);
-              
-              window.App = App;
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var FirebaseConfig = {
+        apiKey: "AIzaSyA34-tWrMtoke3lP-kap4-B0RlzR0Yw4T0",
+        authDomain: "personality-quiz-projec.firebaseapp.com",
+        projectId: "personality-quiz-projec",
+        storageBucket: "personality-quiz-projec.appspot.com",
+        messagingSenderId: "1002094943929",
+        appId: "1:1002094943929:web:024b65a648c2de91450568"
+        //measurementId: "G-7CLEK2MBTK"
+    };
+  
+    App.FirebaseConfig = FirebaseConfig;
+    // Initialize Firebase
+    firebase.initializeApp(App.FirebaseConfig);
+    console.log('Firebase has been initialized. ')
+
+    window.App = App;
 
 })(window);
